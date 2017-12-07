ff7ff213ce844529ed4ab1648674aff8f8fded11
quininer <quininer@live.com>
2017-12-07 15:15:45 +0800

    update hacl*

--- build.rs
+++ build.rs
@@ -18,9 +18,9 @@ fn main() {
                 .header($input)
                 .ctypes_prefix("::libc")
                 .use_core()
-                .whitelisted_type($white)
-                .whitelisted_function($white)
-                .whitelisted_var($white)
+                .whitelist_type($white)
+                .whitelist_function($white)
+                .whitelist_var($white)
                 .generate().unwrap()
                 .write_to_file(outdir.join($output)).unwrap();
         };
@@ -32,19 +32,19 @@ fn main() {
     }
 
     bindgen!{
-        "hacl-star/snapshots/hacl-c/Salsa20.h"          => "salsa20.rs",            "Salsa20_.+";
-        "hacl-star/snapshots/hacl-c/Chacha20.h"         => "chacha20.rs",           "Chacha20_.+";
-        "hacl-star/snapshots/hacl-c/Poly1305_64.h"      => "poly1305.rs",           "Poly1305_.+";
-        "hacl-star/snapshots/hacl-c/AEAD_Poly1305_64.h" => "aead_poly1305.rs",      "AEAD_Poly1305_.+";
-        "hacl-star/snapshots/hacl-c/HMAC_SHA2_256.h"    => "hmac_sha2_256.rs",      "hmac.*";
-        "hacl-star/snapshots/hacl-c/SHA2_256.h"         => "sha2_256.rs",           "SHA2_256_.+";
-        "hacl-star/snapshots/hacl-c/SHA2_384.h"         => "sha2_384.rs",           "SHA2_384_.+";
-        "hacl-star/snapshots/hacl-c/SHA2_512.h"         => "sha2_512.rs",           "SHA2_512_.+";
-        "hacl-star/snapshots/hacl-c/Ed25519.h"          => "ed25519.rs",            "Ed25519_.+";
-        "hacl-star/snapshots/hacl-c/Curve25519.h"       => "curve25519.rs",         "Curve25519_.+";
-        "hacl-star/snapshots/hacl-c/Chacha20Poly1305.h" => "chacha20poly1305.rs",   "Chacha20Poly1305_.+";
-        "hacl-star/snapshots/hacl-c/Hacl_Policies.h"    => "hacl_policies.rs",      "Hacl_Policies_.+";
-        "hacl-star/snapshots/hacl-c/NaCl.h"             => "nacl.rs",               "NaCl_.+"
+        "hacl-star/snapshots/hacl-c/AEAD_Poly1305_64.h"      => "aead_poly1305.rs",      "AEAD_Poly1305_.+";
+        "hacl-star/snapshots/hacl-c/Hacl_Salsa20.h"          => "salsa20.rs",            "Hacl_Salsa20_.+";
+        "hacl-star/snapshots/hacl-c/Hacl_Chacha20.h"         => "chacha20.rs",           "Hacl_Chacha20_.+";
+        "hacl-star/snapshots/hacl-c/Hacl_Poly1305_64.h"      => "poly1305.rs",           "Hacl_Poly1305_.+";
+        "hacl-star/snapshots/hacl-c/Hacl_HMAC_SHA2_256.h"    => "hmac_sha2_256.rs",      "hmac.*";
+        "hacl-star/snapshots/hacl-c/Hacl_SHA2_256.h"         => "sha2_256.rs",           "Hacl_SHA2_256_.+";
+        "hacl-star/snapshots/hacl-c/Hacl_SHA2_384.h"         => "sha2_384.rs",           "Hacl_SHA2_384_.+";
+        "hacl-star/snapshots/hacl-c/Hacl_SHA2_512.h"         => "sha2_512.rs",           "Hacl_SHA2_512_.+";
+        "hacl-star/snapshots/hacl-c/Hacl_Ed25519.h"          => "ed25519.rs",            "Hacl_Ed25519_.+";
+        "hacl-star/snapshots/hacl-c/Hacl_Curve25519.h"       => "curve25519.rs",         "Hacl_Curve25519_.+";
+        "hacl-star/snapshots/hacl-c/Hacl_Chacha20Poly1305.h" => "chacha20poly1305.rs",   "Hacl_Chacha20Poly1305_.+";
+        "hacl-star/snapshots/hacl-c/Hacl_Policies.h"         => "hacl_policies.rs",      "Hacl_Policies_.+";
+        "hacl-star/snapshots/hacl-c/NaCl.h"                  => "nacl.rs",               "NaCl_.+"
     };
 
     println!("cargo:rustc-link-search=native={}/build", dst.display());
