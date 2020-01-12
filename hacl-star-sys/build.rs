0af076f6b6998b0b9da2d068a97663141fee2841
quininer <quininer@live.com>
2020-01-12 23:33:54 +0800

    Switch to hacl-star 0.2

--- hacl-star-sys/build.rs
+++ hacl-star-sys/build.rs
@@ -1,102 +1,111 @@
-extern crate cc;
-#[cfg(feature = "bindgen")] extern crate bindgen;
+use std::{ fs, env };
+use std::path::Path;
+use std::borrow::Cow;
 
-use std::env;
-#[cfg(feature = "bindgen")] use std::path::PathBuf;
 
+const CODE_TARGET: &str = "c89-compatible";
+const CFLAGS: &str = "-Wall -Wextra -Werror -std=c11 -Wno-unused-variable \
+    -Wno-unknown-warning-option -Wno-unused-but-set-variable \
+    -Wno-unused-parameter -Wno-infinite-recursion \
+    -g -fwrapv -D_BSD_SOURCE -D_DEFAULT_SOURCE";
+const CFLAGS2: &str = "-Wno-parentheses -Wno-deprecated-declarations -g -std=gnu11 -O3";
+
+
+fn parse_include(input: &str) -> Option<(&str, &str, &str, &str, &str)> {
+    let mut lines = input.lines().take(5);
+
+    macro_rules! parse {
+        ( $input:expr, $key:expr ) => {{
+            let mut input = $input.next()?.splitn(2, '=');
+            assert_eq!(input.next()?, $key);
+            input.next()?
+        }}
+    }
+
+    Some((
+        parse!(lines, "USER_TARGET"),
+        parse!(lines, "USER_CFLAGS"),
+        parse!(lines, "USER_C_FILES"),
+        parse!(lines, "ALL_C_FILES"),
+        parse!(lines, "ALL_H_FILES"),
+    ))
+}
+
+fn make_evercrypt(
+    code_target: &str,
+    user_target: &str,
+    user_cflags: &str,
+    user_c_files: &str,
+    all_c_files: &str
+) {
+    let mut builder = cc::Build::new();
+
+    // include kremlin
+    builder
+        .include("./hacl-star/dist/kremlin/include")
+        .include("./hacl-star/dist/kremlin/kremlib/dist/minimal")
+        .include(format!("./hacl-star/dist/{}", code_target));
+
+
+    for flag in CFLAGS.split(' ').chain(user_cflags.split(' ')).chain(CFLAGS2.split(' ')) {
+        builder.flag_if_supported(flag.trim());
+    }
+
+    for file in all_c_files.split(' ').chain(user_c_files.split(' ')) {
+        builder.file(format!("./hacl-star/dist/{}/{}", code_target, file.trim()));
+    }
+
+    match env::var("CARGO_CFG_TARGET_FAMILY").as_ref().map(String::as_str) {
+        Ok("unix") => {
+            builder
+                .flag_if_supported("-fPIC")
+                .flag_if_supported("-fstack-check");
+        },
+        Ok("windows") => {
+            builder
+                .flag("-D__USE_MINGW_ANSI_STDIO")
+                .flag("-fno-asynchronous-unwind-tables");
+        },
+        _ => ()
+    }
+
+    builder.compile(user_target.trim_start_matches("lib").trim_end_matches(".a"));
+}
+
+fn make_binding(code_target: &str) {
+    let outdir = env::var("OUT_DIR").unwrap();
+    let outdir = Path::new(&outdir);
+
+    let mut builder = bindgen::Builder::default();
+
+    if env::var("CARGO_CFG_TARGET_ARCH") == Ok("wasm".into()) {
+        builder = builder.clang_arg("-fvisibility=default");
+    }
+
+    builder
+        .clang_arg("-I./hacl-star/dist/kremlin/include")
+        .clang_arg("-I./hacl-star/dist/kremlin/kremlib/dist/minimal")
+        .clang_arg(format!("-I./hacl-star/dist/{}", code_target))
+        .header(format!("./hacl-star/dist/{}/EverCrypt.h", code_target))
+        .header(format!("./hacl-star/dist/{}/Hacl_NaCl.h", code_target))
+        .ctypes_prefix("crate::libc")
+        .whitelist_function("EverCrypt.*|Hacl.*")
+        .whitelist_type("EverCrypt.*|Hacl.*")
+        .use_core()
+        .generate().unwrap()
+        .write_to_file(outdir.join("sys.rs")).unwrap();
+}
 
 fn main() {
-    let mut cc = cc::Build::new();
+    let code_target = env::var("HACL_CODE_TARGET")
+        .map(Cow::Owned)
+        .unwrap_or(Cow::Borrowed(CODE_TARGET));
 
-    // from https://github.com/project-everest/hacl-star/blob/master/snapshots/makefiles/CMakeLists.txt#L62
-    if env::var("CARGO_CFG_TARGET_POINTER_WIDTH") == Ok("32".into())
-        || env::var("CARGO_CFG_TARGET_ENV") == Ok("msvc".into())
-    {
-        cc
-            .shared_flag(true)
-            .define("KRML_NOUINT128", None)
-            .flag_if_supported("-Wno-unused-function")
-            .file("hacl-c/FStar.c");
-    }
+    let include =
+        fs::read_to_string(format!("./hacl-star/dist/{}/Makefile.include", code_target)).unwrap();
+    let (user_target, user_cflags, user_c_files, all_c_files, _) =
+        parse_include(&include).expect("makefile.include parse failed");
 
-    cc
-        .flag_if_supported(
-            if cc::Build::new().get_compiler().is_like_gnu() || cc::Build::new().get_compiler().is_like_clang()
-            { "-std=gnu11" } else { "-std=c11" }
-        )
-        .include("hacl-c")
-
-        // from https://github.com/mitls/hacl-star/blob/master/snapshots/hacl-c/Makefile#L8
-        .flag_if_supported("-fwrapv")
-        .flag_if_supported("-fomit-frame-pointer")
-        .flag_if_supported("-funroll-loops")
-
-        .files(&[
-            "hacl-c/Hacl_Salsa20.c",
-            "hacl-c/Hacl_Poly1305_32.c",
-            "hacl-c/Hacl_Poly1305_64.c",
-            "hacl-c/Hacl_Chacha20.c",
-            "hacl-c/AEAD_Poly1305_64.c",
-            "hacl-c/Hacl_Chacha20Poly1305.c",
-            "hacl-c/Hacl_HMAC_SHA2_256.c",
-            "hacl-c/Hacl_SHA2_256.c",
-            "hacl-c/Hacl_SHA2_384.c",
-            "hacl-c/Hacl_SHA2_512.c",
-            "hacl-c/Hacl_Ed25519.c",
-            "hacl-c/Hacl_Curve25519.c",
-            "hacl-c/kremlib.c",
-            "hacl-c/Hacl_Policies.c",
-            "hacl-c/NaCl.c"
-        ])
-
-        // ignore some warnings
-        .flag_if_supported("-Wno-unused-parameter")
-        .flag_if_supported("-Wno-unused-variable")
-
-        .compile("hacl");
-
-    #[cfg(all(feature = "bindgen", feature = "overwrite"))]
-    let outdir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
-        .join("src")
-        .join("imp");
-
-    #[cfg(all(feature = "bindgen", not(feature = "overwrite")))]
-    let outdir = PathBuf::from(env::var("OUT_DIR").unwrap());
-
-    #[cfg(feature = "bindgen")]
-    macro_rules! bindgen {
-        ( @bind $input:expr => $output:expr , $white:expr ) => {
-            bindgen::Builder::default()
-                .header($input)
-                .ctypes_prefix("crate::libc")
-                .use_core()
-                .whitelist_type($white)
-                .whitelist_function($white)
-                .whitelist_var($white)
-                .generate().unwrap()
-                .write_to_file(outdir.join($output)).unwrap();
-        };
-        ( $( $input:expr => $output:expr , $white:expr );* ) => {
-            $(
-                bindgen!(@bind $input => $output, $white);
-            )*
-        }
-    }
-
-    #[cfg(feature = "bindgen")]
-    bindgen!{
-        "hacl-c/AEAD_Poly1305_64.h"      => "aead_poly1305.rs",      "AEAD_Poly1305_.+";
-        "hacl-c/Hacl_Salsa20.h"          => "salsa20.rs",            "Hacl_Salsa20_.+";
-        "hacl-c/Hacl_Chacha20.h"         => "chacha20.rs",           "Hacl_Chacha20_.+";
-        "hacl-c/Hacl_Poly1305_64.h"      => "poly1305.rs",           "Hacl_Poly1305_.+";
-        "hacl-c/Hacl_HMAC_SHA2_256.h"    => "hmac_sha2_256.rs",      "hmac.*";
-        "hacl-c/Hacl_SHA2_256.h"         => "sha2_256.rs",           "Hacl_SHA2_256_.+";
-        "hacl-c/Hacl_SHA2_384.h"         => "sha2_384.rs",           "Hacl_SHA2_384_.+";
-        "hacl-c/Hacl_SHA2_512.h"         => "sha2_512.rs",           "Hacl_SHA2_512_.+";
-        "hacl-c/Hacl_Ed25519.h"          => "ed25519.rs",            "Hacl_Ed25519_.+";
-        "hacl-c/Hacl_Curve25519.h"       => "curve25519.rs",         "Hacl_Curve25519_.+";
-        "hacl-c/Hacl_Chacha20Poly1305.h" => "chacha20poly1305.rs",   "Hacl_Chacha20Poly1305_.+";
-        "hacl-c/Hacl_Policies.h"         => "hacl_policies.rs",      "Hacl_Policies_.+";
-        "hacl-c/NaCl.h"                  => "nacl.rs",               "NaCl_.+"
-    };
+    make_evercrypt(&code_target, user_target, user_cflags, user_c_files, all_c_files);
+    make_binding(&code_target);
 }
--- hacl-star-sys/src/lib.rs
+++ hacl-star-sys/src/lib.rs
@@ -3,54 +3,6 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-#[cfg(not(all(target_arch = "wasm32", not(any(target_os = "emscripten", target_os = "wasi")))))]
 use libc;
 
-#[cfg(all(target_arch = "wasm32", not(any(target_os = "emscripten", target_os = "wasi"))))]
-mod libc {
-    pub type c_void = u8;
-    pub type c_int = i32;
-    pub type c_uint = u32;
-    pub type c_ulong = u32;
-    pub type c_uchar = u8;
-}
-
-#[cfg(any(
-    not(feature = "bindgen"),
-    all(feature = "bindgen", feature = "overwrite")
-))]
-mod imp;
-
-#[cfg(all(feature = "bindgen", not(feature = "overwrite")))]
-mod imp {
-    macro_rules! import {
-        ( @import $name:ident ) => {
-            pub mod $name {
-                include!(concat!(env!("OUT_DIR"), "/", stringify!($name), ".rs"));
-            }
-        };
-        ( $( pub mod $name:ident ; )* ) => {
-            $(
-                import!(@import $name);
-            )*
-        }
-    }
-
-    import!{
-        pub mod aead_poly1305;
-        pub mod chacha20poly1305;
-        pub mod salsa20;
-        pub mod chacha20;
-        pub mod poly1305;
-        pub mod hmac_sha2_256;
-        pub mod sha2_256;
-        pub mod sha2_384;
-        pub mod sha2_512;
-        pub mod ed25519;
-        pub mod curve25519;
-        pub mod hacl_policies;
-        pub mod nacl;
-    }
-}
-
-pub use imp::*;
+include!(concat!(env!("OUT_DIR"), "/", "sys", ".rs"));
